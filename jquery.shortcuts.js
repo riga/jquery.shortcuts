5f2336f0257c3aa59afc8cef71d053137c75aa40
Riga <marcelrieger@me.com>
2013-02-21 11:21:08 +0100

    Fix remote and empty function.

--- jquery.shortcuts.js
+++ jquery.shortcuts.js
@@ -15,75 +15,81 @@
 // object cache
 var _shortcuts = {};
 
-jQuery.Shortcuts = function( id ) {
+jQuery.Shortcuts = function(id) {
 
-	var self = id && _shortcuts[ id ];
+    var self = id && _shortcuts[id];
 
-	if ( !self ) {
-		// callback storage for the given id
-		var _callbacks = {},
+    if(!self) {
+        // callback storage for the given id
+        var _callbacks = {},
 
-		// add function, that processes arbitrary callbacks from the arguments
-		add = function( key ) {
-			var args = jQuery.makeArray( arguments );
-			args.shift();
-			_callbacks[ key ] = (_callbacks[key] || jQuery.Callbacks()).add( args );
-			return this;
-		},
+        // add function, that processes arbitrary callbacks from the arguments
+        add = function(key) {
+            var args = jQuery.makeArray(arguments);
+            args.shift();
+            _callbacks[key] = (_callbacks[key] || jQuery.Callbacks()).add(args);
+            return self;
+        },
 
-		// remove function, that processes arbitrary callbacks from the arguments
-		remove = function( key ) {
-			if ( !_callbacks[ key ] ) {
-				return;
-			}
-			var args = jQuery.makeArray( arguments );
-			args.shift();
-			if ( !args.length ) {
-				_callbacks[ key ].empty();
-			} else {
-				_callbacks[ key ].remove( args );
-			}
-			return this;
-		},
+        // remove function, that processes arbitrary callbacks from the arguments
+        remove = function(key) {
+            if(!_callbacks[key]) {
+                return;
+            }
+            var args = jQuery.makeArray(arguments);
+            args.shift();
+            if(!args.length) {
+                _callbacks[key].empty();
+            } else {
+                _callbacks[key].remove(args);
+            }
+            return self;
+        },
 
-		// enable function
-		// keys can be passed in the arguments
-		enable = function() {
-			var args = jQuery.makeArray( arguments );
-			this.disable.apply( this, args );
-			jQuery.each( _callbacks, function( key, callback ) {
-				if ( args.length === 0 || jQuery.inArray( key, args ) > -1 ) {
-					// use name spaces to use callbacks more than once
-					jQuery(document).bind( 'keydown.' + key.replace( /\+/g, '' ), key, callback.fire );
-				}
-			});
-			return this;
-		},
+        empty = function() {
+            _callbacks = {};
+            return self;
+        },
 
-		// disable function
-		// keys can be passed in the arguments
-		disable = function() {
-			var args = jQuery.makeArray( arguments );
-			jQuery.each( _callbacks, function( key, callback ) {
-				if ( args.length === 0 || jQuery.inArray( key, args ) > -1 ) {
-					// use namespaces to use callbacks more than once
-					jQuery(document).unbind( 'keydown.' + key.replace( /\+/g, '' ), callback.fire );
-				}
-			});
-			return this;
-		};
+        // enable function
+        // keys can be passed in the arguments
+        enable = function() {
+            var args = jQuery.makeArray(arguments);
+            this.disable.apply(this, args);
+            jQuery.each(_callbacks, function(key, callback) {
+                if(args.length === 0 || jQuery.inArray(key, args) > -1) {
+                    // use name spaces to use callbacks more than once
+                    jQuery(document).bind('keydown.' + key.replace(/\+/g, ''), key, callback.fire);
+                }
+            });
+            return self;
+        },
 
-		self = {
-			add: add,
-			remove: remove,
-			enable: enable,
-			disable: disable
-		};
+        // disable function
+        // keys can be passed in the arguments
+        disable = function() {
+            var args = jQuery.makeArray(arguments);
+            jQuery.each(_callbacks, function(key, callback) {
+                if(args.length === 0 || jQuery.inArray(key, args) > -1) {
+                    // use namespaces to use callbacks more than once
+                    jQuery(document).unbind('keydown.' + key.replace(/\+/g, ''), callback.fire);
+                }
+            });
+            return self;
+        };
 
-		if ( id ) {
-			_shortcuts[ id ] = self;
-		}
-	}
+        self = {
+            add: add,
+            remove: remove,
+            empty: empty,
+            enable: enable,
+            disable: disable
+        };
 
-	return self;
+        if(id) {
+            _shortcuts[id] = self;
+        }
+    }
+
+    return self;
 };
